7d36cff1628226a538758b94f105452b26c823f1
japvidal <japvidal@gmail.com>
2026-04-03 12:13:14 +0200

    feat: add method tracing logs

--- src/main/java/com/microservicios/app/futfem/teams/services/TeamServiceImpl.java
+++ src/main/java/com/microservicios/app/futfem/teams/services/TeamServiceImpl.java
@@ -3,6 +3,8 @@ package com.microservicios.app.futfem.teams.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.microservicios.app.common.services.CommonServiceImpl;
@@ -11,6 +13,8 @@ import com.microservicios.app.futfem.teams.models.repository.TeamRepository;
 
 @Service
 public class TeamServiceImpl extends CommonServiceImpl<Team, TeamRepository> implements TeamService {
+
+	private static final Logger log = LoggerFactory.getLogger(TeamServiceImpl.class);
 
 	@Override
 	public Optional<Team> findByNameContainingAndCountry(String name, String country) {
@@ -26,6 +30,7 @@ public class TeamServiceImpl extends CommonServiceImpl<Team, TeamRepository> imp
 	}
 
 	private String normalize(String value) {
+		log.debug("Init method TeamServiceImpl.normalize");
 		if (value == null) {
 			return null;
 		}
